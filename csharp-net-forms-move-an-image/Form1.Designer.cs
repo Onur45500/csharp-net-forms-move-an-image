eab09df93e65e0d3751762f3a5e4a1271de24c39
Onur45500 <onur.akmese@viacesi.fr>
2024-10-11 09:44:51 +0200

    Set up the variables of the project

--- csharp-net-forms-move-an-image/Form1.Designer.cs
+++ csharp-net-forms-move-an-image/Form1.Designer.cs
@@ -29,11 +29,48 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
+            label1 = new Label();
+            FormTimer = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Yellow;
+            label1.Location = new Point(12, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(208, 38);
+            label1.TabIndex = 0;
+            label1.Text = "Move an image";
+            // 
+            // FormTimer
+            // 
+            FormTimer.Enabled = true;
+            FormTimer.Interval = 20;
+            FormTimer.Tick += FormTimerEvent;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            DoubleBuffered = true;
+            Name = "Form1";
             Text = "Form1";
+            Paint += FormPaint;
+            MouseDown += FormMouseDown;
+            MouseMove += FormMouseMove;
+            MouseUp += FormMouseUp;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private System.Windows.Forms.Timer FormTimer;
     }
 }
--- csharp-net-forms-move-an-image/Form1.cs
+++ csharp-net-forms-move-an-image/Form1.cs
@@ -2,9 +2,40 @@ namespace csharp_net_forms_move_an_image
 {
     public partial class Form1 : Form
     {
+        Image logo;
+        Point position = new Point(200, 200);
+        bool dragging;
+        Rectangle rect;
+        int height = 200, width = 100;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void FormMouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void FormMouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void FormMouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void FormPaint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void FormTimerEvent(object sender, EventArgs e)
+        {
+
         }
     }
 }
